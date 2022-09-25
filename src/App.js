eb034578c6f9153db677b2914f1682c0c776a69c
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-09-25 11:13:48 -0300

    basic css maded

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import './App.css';
 function App() {
   
   // Task State
-  const [todo,setTodo] = useState([
-    {"id": 1, "title" : "Task", "status": false},
-    {"id": 2, "title" : "Task", "status": false}
+  const [toDo,setTodo] = useState([
+    {"id": 1, "title" : " Task", "status": false},
+    {"id": 2, "title" : " Task", "status": false}
   ])
 
   // Temp State
@@ -50,6 +50,32 @@ function App() {
   return (
     <div className="container App">
     <h2>To Do List</h2>
+
+    {toDo && toDo.length ? '' : "No task . . ." }
+
+    {toDo && toDo.map((task, index) => {
+      return <React.Fragment key={task.id}>
+
+      <div className="col taskBg">
+
+        <div className={task.status ? 'done' : ''}>
+          <span className="taskNumber">{index + 1}</span>
+          <span className="taskText">{task.title}</span>
+        </div>
+        <div className="iconsWrap">
+          <span>
+            <FontAwesomeIcon icon={faCircleCheck} onClick={markDone}/>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faPen} onClick={updateTask}/>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faTrashCan} onClick={deleteTask}/>
+          </span>
+        </div>
+      </div>
+      </React.Fragment>
+    })}
     </div>
   );
 }
